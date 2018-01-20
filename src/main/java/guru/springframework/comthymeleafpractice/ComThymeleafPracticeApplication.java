34ad9193a156e52345ffa8668a6add1eb9e6611b
Eric Braun <Eric.Braun@astontech.com>
2018-01-20 12:48:03 -0600

    initial

---
+++ src/main/java/guru/springframework/comthymeleafpractice/ComThymeleafPracticeApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.comthymeleafpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComThymeleafPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComThymeleafPracticeApplication.class, args);
+	}
+}
